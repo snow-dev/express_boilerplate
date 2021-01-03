dbb896fe0b4701c5a90f37defebd1196c2d170d1
Snow Dev <dev.carlos.avila@gmail.com>
2021-01-02 22:41:16 -0600

    REMS-002 0. Added logger middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import * as fs from 'fs';
+import path from 'path';
+import morgan from 'morgan';
 
 /* Import routes */
 import router from './http/routes/v1/index';
@@ -7,10 +9,15 @@ import router from './http/routes/v1/index';
 // Instantiate App
 const app = express();
 
-app.use( express.logger( {
-	format:  'dev',
-	stream: fs.createWriteStream('app.log', {'flags': 'w'})
-} ) )
+// create logger "middleware"
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+// setup the logger
+app.use(morgan('dev', {
+	stream: accessLogStream,
+	
+}));
+
 app.use(express.json());
 
 // We set routes on http/routes/v1/index.js as our main router.
